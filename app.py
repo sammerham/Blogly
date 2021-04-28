8bc299add226b239921a1a5dcfaa401f03dfb717
Matt Riese <rise.matt@gmail.com>
2021-04-28 14:18:46 -0700

    root route, users route, created flask app, base.html, user_list.html

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def index():
+    """redirect to list of users"""
+
+    return redirect("/users")
+
+@app.route("/users")
+def list_users():
+    """List users and show add user button."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
