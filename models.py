58bccc3917edda8e764975c11da6aa8e3e351acf
Matt Riese <rise.matt@gmail.com>
2021-04-29 15:24:34 -0700

    added posts table from part 2

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy #, DateTime
+import datetime
 db = SQLAlchemy()
 
 
@@ -25,3 +26,22 @@ class User(db.Model):
                      nullable=False,
                      unique=False)
     image_url = db.Column(db.Text, nullable=True)
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    title = db.Column(db.String(100),
+                    nullable=False,
+                    unique=False)
+    content = db.Column(db.Text,
+                    nullable=False,
+                    unique=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow,
+                    nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
